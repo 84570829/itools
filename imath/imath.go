706e039f89f8718bc26d55c9c454c8c2bfd011cd
samir <84570829@qq.com>
2023-03-31 14:20:48 +0800

    删除iencrypt，新增获取随机字符串函数

--- imath/imath.go
+++ imath/imath.go
@@ -2,8 +2,40 @@ package imath
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
+	"time"
+	"unsafe"
 )
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+const (
+	letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	// 6 bits to represent a letter index
+	letterIdBits = 6
+	// All 1-bits as many as letterIdBits
+	letterIdMask = 1<<letterIdBits - 1
+	letterIdMax  = 63 / letterIdBits
+)
+
+// RandStr 获取随机字符串
+func RandStr(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdMax letters!
+	for i, cache, remain := n-1, src.Int63(), letterIdMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdMax
+		}
+		if idx := int(cache & letterIdMask); idx < len(letters) {
+			b[i] = letters[idx]
+			i--
+		}
+		cache >>= letterIdBits
+		remain--
+	}
+	return *(*string)(unsafe.Pointer(&b))
+}
 
 // Decimal 保留n位小数
 func Decimal(value float64, length int) (res float64) {
